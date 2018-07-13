fbfa2a609cfe4e217609977a4c7b7077765bef4f
sastolarski <sastolarski@gmail.com>
2018-07-13 17:28:37 -0500

    stuff

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
 INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
 INSERT INTO burgers (burger_name) VALUES ('Barbeque Chicken Burger');
-
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', true);
