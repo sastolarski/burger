abf4b8a668d8f673cf2aea38ed908a3eafd83dd5
sastolarski <sastolarski@gmail.com>
2018-07-13 20:25:27 -0500

    still no put

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -18,7 +18,7 @@ router.post('/api/burgers', function(req, res) {
   ], [
     req.body.burger_name
   ], function(data) {
-    res.redirect('/');
+    es.status(200).end();
   });
 });
 
@@ -28,7 +28,7 @@ router.put('/api/burgers/:id', function(req, res) {
   burger.updateOne({
     devoured: true
   }, condition, function(data) {
-    res.redirect('/');
+    es.status(200).end();
   });
 });
 
